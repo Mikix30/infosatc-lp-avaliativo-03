51020f7deebdcb5a798194966ac1bad451f5b3b1
Júlia Barboza Brunelli <69260646+Mikix30@users.noreply.github.com>
2020-09-17 15:14:16 -0300

    Feat: Adiciona Exercicío 02

---
+++ exrcicios/exercicio02.py
@@ -0,0 +1,8 @@
+
+#Atividade número dois
+#Calcule o somatório dos números de 1 a 100 e imprima o resultado.
+
+numb = 1
+while numb < 101:
+    print("A somatória de 1 a 100: ", numb)
+    numb = numb + 1
